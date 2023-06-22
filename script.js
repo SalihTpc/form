40ae0c8ed3872a0d394c389c64362ee0dcefa610
SalihTpc <salihtopcu@hotmail..com.tr>
2023-06-22 14:14:56 +0300

    some changes

--- script.js
+++ script.js
@@ -10,43 +10,45 @@ btn.addEventListener("click", () => {
   const kurulum = inputs.elements["kurulum"].value.trim();
   const kabin = inputs.elements["kabin"].value.trim();
 
-  if (!mail.length > 0) {
-    alert("Email giriniz.");
-    return;
-  }
-  console.log(marka, mail, hdd, ram, core, kurulum, kabin);
+  // if (!mail.length > 0) {
+  //   alert("Email giriniz.");
+  //   return;
+  // }
+  // console.log(marka, mail, hdd, ram, core, kurulum, kabin);
 
-  Email.send({
-    SecureToken: " 826c71eb-00f5-4f09-a65f-5b12473f5e5d",
-    Host: "smtp.elasticemail.com",
-    Username: mailAddress,
-    Password: "400E434711073A0AF1942DFB608A5D8F6A15",
-    To: mailAddress,
-    From: mailAddress,
-    Subject: "itnoya.com için hazırlanmıştır.",
-    Body:
-      "Marka Tercihi: " +
-      marka +
-      "<br>" +
-      "Hdd Tipi:" +
-      hdd +
-      "<br>" +
-      "Ram:" +
-      ram +
-      "<br>" +
-      "Fiziksel Core:" +
-      core +
-      "<br>" +
-      "Koruyucu server kabin:" +
-      kabin +
-      "<br>" +
-      "Kurulum:" +
-      kurulum +
-      "<br>" +
-      "Mail Adresi:" +
-      mail,
-  }).then((msg) => {
-    alert("The email successfully sent");
-    inputs.elements["mail"].value = "";
-  });
+  if (mail.length > 0) {
+    Email.send({
+      SecureToken: " 826c71eb-00f5-4f09-a65f-5b12473f5e5d",
+      Host: "smtp.elasticemail.com",
+      Username: mailAddress,
+      Password: "400E434711073A0AF1942DFB608A5D8F6A15",
+      To: mailAddress,
+      From: mailAddress,
+      Subject: "itnoya.com için hazırlanmıştır.",
+      Body:
+        "Marka Tercihi: " +
+        marka +
+        "<br>" +
+        "Hdd Tipi:" +
+        hdd +
+        "<br>" +
+        "Ram:" +
+        ram +
+        "<br>" +
+        "Fiziksel Core:" +
+        core +
+        "<br>" +
+        "Koruyucu server kabin:" +
+        kabin +
+        "<br>" +
+        "Kurulum:" +
+        kurulum +
+        "<br>" +
+        "Mail Adresi:" +
+        mail,
+    }).then((msg) => {
+      alert("The email successfully sent");
+      inputs.elements["mail"].value = "";
+    });
+  }
 });
